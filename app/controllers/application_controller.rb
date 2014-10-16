97c4ca6f86983210b1d79387701e21f231fa18f4
Steven Doll <steven@rigil.com>
2014-10-16 01:02:15 -0400

    ME-124, ME-140 added token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
+  # authorization
+  include Pundit
+  
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # api behavior
+  # see http://www.soryy.com/ruby/api/rails/authentication/2014/03/16/apis-with-devise.html
+  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
 end
