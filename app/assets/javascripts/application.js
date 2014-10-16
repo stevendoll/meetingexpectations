d5ade2ac966d49e1e386ca4725789b144d118c95
Steven Doll <steven@rigil.com>
2014-10-15 23:34:34 -0400

    ME-122 inspinia theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,25 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require plugins/metisMenu/jquery.metisMenu
+//= require plugins/slimscroll/jquery.slimscroll.min
+//= require plugins/flot/jquery.flot
+//= require plugins/flot/jquery.flot.tooltip.min
+//= require plugins/flot/jquery.flot.spline
+//= require plugins/flot/jquery.flot.resize
+//= require plugins/flot/jquery.flot.pie
+//= require plugins/peity/jquery.peity.min
+//= require demo/peity-demo
+//= plugins/pace/pace.min
+//= plugins/jquery-ui/jquery-ui.min
+//= plugins/gritter/jquery.gritter.min
+//= plugins/easypiechart/jquery.easypiechart
+//= plugins/sparkline/jquery.sparkline.min
+//= demo/sparkline-demo
+//= plugins/chartJs/Chart.min
+
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require inspinia
+//= require jquery-ui.custom.min
 //= require_tree .
