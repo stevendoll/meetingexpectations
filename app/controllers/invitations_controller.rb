2ca8157789dbd74b5c4fe864a81ecaadef445088
Steven Doll <steven@rigil.com>
2014-10-21 09:07:03 -0400

    ME-147 removed extra devise controllers

--- app/controllers/invitations_controller.rb
+++
@@ -1,8 +0,0 @@
-class InvitationsController < Devise::InvitationsController
-  before_filter :update_sanitized_params, if: :devise_controller?
-
-  def update_sanitized_params
-    devise_parameter_sanitizer.for(:accept_invitation) {|u| u.permit(:name, :email, :password, :password_confirmation, :first_name, :last_name, :phone, :avatar, :description, :invitation_code, :invitation_token)} 
-  end
-
-end
